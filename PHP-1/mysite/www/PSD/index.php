ff2c6ddc6b6d95c38edda9fc5ae7ddbd961913c3
Andrei <rabota.laptev@mail.ru>
2018-10-09 14:08:44 +0700

    Верстка макета с использованием bootstrap сетки 1200/12

--- PHP-1/mysite/www/PSD/index.php
+++ PHP-1/mysite/www/PSD/index.php
@@ -5,71 +5,50 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>First test PSD</title>
+    <title>PSD макет лендинга</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class="container ">
-    <div class="header">
-
-            <a href="#">
-                <img class="logo" src="img/logo.png" alt="logo">
-            </a>
+<div class="container-fluid header">
+    <div class="container header">
+        <div class="row">
+            <div class="logo col-lg-1">
+                <img src="img/logo.png" alt="">
+            </div>
+            <div class="menu">
                 <ul>
-                <a href="#">
-                    <li>HOME</li>
-                </a><a href="about.html">
-                <li>ABOUT</li>
-            </a><a href="contact.html">
-                <li>CONTACT</li>
-            </a>
-            </ul>
-
-
-
-
-    </div>
-    <div class="slogan">
-        <h1>UI/UX & Graphic Designer</h1>
-        <p>I am a Graphic & Web Designer based in New York, specializing <br>
-            in User Interface Design and Development.</p>
-    </div>
-</div>
-
-<div class="container">
-    <div class="gallery">
-<!--        --><?php //require 'gallery.php';?>
-        <div class="row">
-
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
+                    <a href="#"><li> <a href="#">HOME</li></a>
+                    <a href="#"><li>ABOUT</li> </a>
+                    <li>WORK</li>
+                    <li>PROCESS</li>
+                    <li>SERVICES</li>
+                    <li>TESTIMONIALS</li>
+                    <li>CONTACT</li>
+                </ul>
+            </div>
         </div>
         <div class="row">
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-        </div>
-        <div class="row">
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
-            <div class="img"></div>
+            <div class="slogan col-lg-6 col-lg-offset-6">
+                <h1>We Design and Develop</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur at cum, est in minus nihil vitae. Debitis nisi, recusandae!</p>
+                <input type="button" class="headerBtn" value="CONTACT US">
+            </div>
         </div>
     </div>
-</div>
-<div class="container">
-    <div class="footer">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-</div>
+    <div class="container-fluid aboutUs">
+        <div class="col-lg-8 col-lg-offset-2">
+            <h1>
+                About US
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto debitis doloribus error nemo neque officiis, placeat reiciendis sint soluta voluptas. Autem dolor earum facere illum necessitatibus pariatur praesentium saepe tenetur! Aperiam consectetur deleniti dolore dolores eos ex molestias non reiciendis!
+            </p>
+            <img src="img/signature.png" alt="Роспись">
+        </div>
 
-<script src="script.js"></script>
+    </div>
+
+</div>
 </body>
 </html>
