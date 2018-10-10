f6ff033749d9fd2fdfe9782bb77cf63850f04ea1
Andrei <rabota.laptev@mail.ru>
2018-10-10 18:46:55 +0700

    работа с макетом

--- PHP-1/mysite/www/PSD/index.php
+++ PHP-1/mysite/www/PSD/index.php
@@ -13,10 +13,10 @@
 <div class="container-fluid header">
     <div class="container header">
         <div class="row">
-            <div class="logo col-lg-1">
+            <div class="logo col-lg-1 col-md-1 col-sm-1">
                 <img src="img/logo.png" alt="">
             </div>
-            <div class="menu">
+            <div class="menu col-lg-11 col-md-11 col-sm-11">
                 <ul>
                     <a href="#"><li> <a href="#">HOME</li></a>
                     <a href="#"><li>ABOUT</li> </a>
@@ -29,26 +29,169 @@
             </div>
         </div>
         <div class="row">
-            <div class="slogan col-lg-6 col-lg-offset-6">
+            <div class="slogan col-lg-6 col-lg-offset-6 col-md-6 col-md-offset-6">
                 <h1>We Design and Develop</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur at cum, est in minus nihil vitae. Debitis nisi, recusandae!</p>
                 <input type="button" class="headerBtn" value="CONTACT US">
             </div>
         </div>
     </div>
-    <div class="container-fluid aboutUs">
-        <div class="col-lg-8 col-lg-offset-2">
-            <h1>
-                About US
-            </h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto debitis doloribus error nemo neque officiis, placeat reiciendis sint soluta voluptas. Autem dolor earum facere illum necessitatibus pariatur praesentium saepe tenetur! Aperiam consectetur deleniti dolore dolores eos ex molestias non reiciendis!
-            </p>
-            <img src="img/signature.png" alt="Роспись">
-        </div>
-
-    </div>
-
 </div>
+<div class="container-fluid aboutUs">
+    <div class="col-lg-8 col-lg-offset-2">
+        <h1>
+            About US
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto debitis doloribus error nemo neque officiis, placeat reiciendis sint soluta voluptas. Autem dolor earum facere illum necessitatibus pariatur praesentium saepe tenetur! Aperiam consectetur deleniti dolore dolores eos ex molestias non reiciendis!
+        </p>
+        <img src="img/signature.png" alt="Роспись">
+    </div>
+</div>
+
+<div class="container-fluid profSkill">
+    <div class="row">
+        <div class="profSkill col-lg-6 col-md-6">
+            <div class=" profScale">
+                <h1>
+                    Professional Skills
+                </h1>
+                <div>
+                    <p>Lorem ipsum dolor sit.</p>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor.</p>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 profImg"></div>
+    </div>
+</div>
+<section class="count">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6 countDiv">
+                <p>548</p>
+                sadfadasdfadfadf
+            </div>
+            <div class="col-lg-3 col-md-3  col-sm-6  countDiv">
+                <p>1265</p>
+                sadfadasdfadfadf
+            </div>
+            <div class="col-lg-3 col-md-3  col-sm-6  countDiv"><p>45</p>
+                sadfadasdfadfadf
+            </div>
+            <div class="col-lg-3 col-md-3  col-sm-6  countDiv">
+                <p>58</p>
+                sadfadasdfadfadf
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="gallery">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="galleryImg col-lg-3 col-md-4 col-sm-6"></div>
+            <div class="galleryImg col-lg-3 col-md-4 col-sm-6"></div>
+            <div class="galleryImg col-lg-3 col-md-4 col-sm-6"></div>
+            <div class="galleryImg col-lg-3 col-md-4 col-sm-6"></div>
+            <div class="galleryImg col-lg-3 col-md-4 col-sm-6"></div>
+            <div class="galleryImg col-lg-3 col-md-4 col-sm-6"></div>
+            <div class="galleryImg col-lg-3 col-md-4 col-sm-6"></div>
+            <div class="galleryImg col-lg-3 col-md-4 col-sm-6"></div>
+        </div>
+        <div class="row">
+            <div class="loadMore col-lg-12">
+                LOAD MORE WORK
+            </div>
+        </div>
+    </div>
+</section>
+<section class="ourWorkProcess">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Our Work Process</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, quisquam. <br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <div class="col-lg-12 videoBloc">
+                    <iframe width="100%" height="100%1" src="https://www.youtube.com/embed/xFa2_PVMeDQ" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="advantages">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="icon"></div>
+                <h4>Lorem ipsum.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam.</p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="icon"></div>
+                <h4>Lorem ipsum.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam.</p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="icon"></div>
+                <h4>Lorem ipsum.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam.</p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="icon"></div>
+                <h4>Lorem ipsum.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam.</p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="icon"></div>
+                <h4>Lorem ipsum.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam.</p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="icon"></div>
+                <h4>Lorem ipsum.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam.</p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="icon"></div>
+                <h4>Lorem ipsum.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam.</p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="icon"></div>
+                <h4>Lorem ipsum.</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="clients">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eius exercitationem modi nihil,
+                    <br> quae reiciendis. Atque autem consequuntur dolorum ea eos
+                    <br> hic iste reprehenderit sed sequi, soluta ullam, voluptates voluptatum.
+                </h2>
+                <p>Lorem ipsum.</p>
+                <ul>
+                    <li>*</li>
+                    <li>*</li>
+                    <li>*</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 </body>
 </html>
